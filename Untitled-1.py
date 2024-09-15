ecfb289e0a66f91b9ca10cf801f552cc0945f345
jakubp11 <jakubpluciennik07@gmail.com>
2024-09-15 13:59:46 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,118 @@
+import math
+PI = math.pi
+print("a - pk b-prt c-tr d-pt e-rtr f-pcku g-pcsz h-pcpr i-pk j-pr k-prów l-vsz ł-vpr n-przk m-vku o-vw p-obkir r-obrtr aa-ppst bb-ppw cc-ppost dd-ppgr ee-vostr ff-vgra gg-obprirów hh-obtrap ll-obtr")
+inp = input()
+if "a" == inp:
+    a = float(input("a = "))
+    print(a*a)
+elif "b" == inp:
+    a = float(input("a = "))
+    b = float(input("b = "))
+    print(a*b)
+elif "c" == inp:
+    a = float(input("a = "))
+    h = float(input("h = "))
+    print(a*h/2)
+elif "d" == inp:
+    a= float(input("a = "))
+    b = float(input("b = "))
+    h = float(input("h = "))
+    print(((a+b)*h)/2)
+elif "e" == inp:
+    a = float(input("a = ")) 
+    print(((a**2)*math.sqrt(3))/4)
+elif "f" == inp:
+    r = float(input("r = ")) 
+    print(4*(math.pi)*(r**2))
+elif "g" == inp:
+    a = float(input("a = ")) 
+    print(6*(a**2))
+elif "h" == inp:
+    a= float(input("a = "))
+    b = float(input("b = "))
+    c = float(input("c = "))
+    print(2*a*b+2*a*c+2*c*b)
+elif "i" == inp:
+    r = float(input("r = "))
+    print(PI*r**2)
+elif "j" == inp:
+    e = float(input("e = "))
+    f = float(input("f = "))
+    print((e*f)/2)
+elif "k" == inp:
+    a = float(input("a = "))
+    h = float(input("h = "))
+    print(a*h)
+elif "l" == inp:
+    a = float(input("a = ")) 
+    print(a**3)
+elif "ł" == inp:
+    a = float(input("a = "))
+    b = float(input("b = "))
+    c = float(input("c = "))
+    print(a*b*c)
+elif "n" == inp:
+    a = float(input("a = ")) 
+    print(a*(math.sqrt(2)))
+elif "m" == inp:
+    r = float(input("r = ")) 
+    print((4/3)*PI*r**3)
+elif "o" == inp:
+    r = float(input("r = "))
+    H = float(input("r = ")) 
+    print(PI*r**2*H)
+elif "p" == inp:
+    a = float(input("a = ")) 
+    print(4*a)
+elif "r" == inp:
+    a= float(input("a = ")) 
+    print(3*a)
+elif "aa" == inp:
+    r = float(input("r = "))
+    l = float(input("l = "))
+    print(PI*r**2+PI*r*l)
+elif "bb" == inp:
+    r = float(input("r = "))
+    H = float(input("H = "))
+    print(2*PI*r**2+2*PI*r*H)
+elif "cc" == inp:
+    Pp = float(input("Pp = "))
+    Pb = float(input("Pb = "))
+    print(Pp+Pb)
+elif "dd" == inp:
+    Pp = float(input("Pp = "))
+    Pb = float(input("Pb = "))
+    print(2*Pp+Pb)
+elif "ee" == inp:
+    Pp = float(input("Pp = "))
+    H = float(input("H = "))
+    print((1/3)*Pp*H)
+elif "ff" == inp:
+    Pp = float(input("Pp = "))
+    H = float(input("H = "))
+    print(Pp*H)
+elif "ff" == inp:
+    r = float(input("r = "))
+    print(2*PI*r**2)
+elif "gg" == inp:
+    a = float(input("a = "))
+    b = float(input("b = "))
+    print(2*a+2*b)
+elif "hh" == inp:
+    a = float(input("a = "))
+    b = float(input("b = "))
+    c = float(input("c = "))
+    d = float(input("d = "))
+    print(a+b+c+d)
+elif "ll" == inp:
+    a = float(input("a = "))
+    b = float(input("b = "))
+    c = float(input("c = "))
+    print(a+b+c)
+else:
+    print("nie ma takiej komendy")
+
+
+
+
+
